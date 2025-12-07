7159dd6b4ad21172481c11d345951869c45dfbee
artur <arturaubdullin@gmail.com>
2025-12-07 18:17:52 +0300

    feat: implement repository
    -Add mapper to map dto into entities for room to use
    -Add NewsRepositoryImpl
    -Bind NewsRepositoryImpl with NewsRepository interface in DI module

---
+++ app/src/main/java/com/example/news/data/mapper/NewsMapper.kt
@@ -0,0 +1,41 @@
+package com.example.news.data.mapper
+
+import com.example.news.data.local.ArticlesDbModel
+import com.example.news.data.remote.NewsResponseDto
+import com.example.news.domain.entity.Article
+import java.text.SimpleDateFormat
+import java.util.Locale
+
+
+fun NewsResponseDto.toDbModels(topic: String): List<ArticlesDbModel> {
+    return articles.map { dto ->
+        ArticlesDbModel(
+            title = dto.title,
+            description = dto.description,
+            url = dto.url,
+            imageUrl = dto.urlToImage,
+            sourceName = dto.source.name,
+            topic = topic,
+            publishedAt = dto.publishedAt.toTimestamp()
+        )
+    }
+}
+
+
+private fun String.toTimestamp(): Long {
+    val dateFormatter = SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss'Z'", Locale.getDefault())
+    return dateFormatter.parse(this)?.time ?: System.currentTimeMillis()
+}
+
+fun List<ArticlesDbModel>.toEntities(): List<Article> {
+    return map {
+        Article(
+            title = it.title,
+            description = it.description,
+            imageUrl = it.imageUrl,
+            sourceName = it.sourceName,
+            publishedAt = it.publishedAt,
+            url = it.url
+        )
+    }.distinct()
+}
