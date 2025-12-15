985dacd4efc1af1ca5651b089d8066feea88cc53
artur <arturaubdullin@gmail.com>
2025-12-15 03:47:22 +0300

    fix: minor changes

--- app/src/main/java/com/example/news/data/remote/NewsApiService.kt
+++ app/src/main/java/com/example/news/data/remote/NewsApiService.kt
@@ -6,7 +6,7 @@ import retrofit2.http.Query
 
 interface NewsApiService {
 
-    @GET("v2/everything?apiKey=5415bf354c9d435a901e0f282521b6d4")
+    @GET("v2/everything?apiKey=${BuildConfig.NEWS_API_KEY}")
     suspend fun loadArticles(
         @Query("q") topic: String,
         @Query("language") language: String
